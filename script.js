0103e85fefbf4fafe61c8bda91e56d0b3feb8f41
Theonlyhamstertoh <theonlyhamstertoh@gmail.com>
2021-03-15 16:36:52 -0700

    added more animation and transition

--- script.js
+++ script.js
@@ -1,44 +1,53 @@
-const cardContainer = document.querySelector('.cardContainer');
-let previousTarget = null;
 
-cardContainer.addEventListener('click', (e) => {
-    if(e.target.id === 'player') {
-        e.target.nextElementSibling.style.color = 'black';
-        e.target.classList.add('addBoxShadow');
-        e.target.style.color = 'red';
-        if(e.target.nextElementSibling.classList.contains('addBoxShadow')) {
-            e.target.nextElementSibling.classList.remove('addBoxShadow');
-            e.target.nextElementSibling.style.color = 'black';
-        }
-      
-    } else if(e.target.id ==='bot') {
-        console.log(e.target.previousElementSibling)
-        e.target.classList.add('addBoxShadow');
-        e.target.style.color = 'red';
-        if(e.target.previousElementSibling.classList.contains('addBoxShadow')) {
-            e.target.previousElementSibling.classList.remove('addBoxShadow');
-            e.target.previousElementSibling.style.color = 'black';
-        }
-  
+const loadingScreen = (() => {
+
+
+    const cardplayer = document.querySelectorAll('.player');
+    const cardbot = document.querySelectorAll('.bot');
+    const iframe1 = document.getElementById('iframe1');
+    const iframe2 = document.getElementById('iframe2');
+    const humanGif = "https://giphy.com/embed/l41lGnxllmN3YqOyI";
+    const botGif = "https://giphy.com/embed/xT77XUw1XMVGIxgove";
+
+    //button animations for loading screen
+    for(let i = 0; i < 2; i++) {
+        cardplayer[i].addEventListener('click', (e) => {
+            e.target.classList.add('keepTransition');
+            chosenPlayer(i, e.target.classList[1]);
+            
+            if(cardbot[i].classList.contains('keepTransition')) {
+                cardbot[i].classList.remove('keepTransition');
+                cardbot[i].classList.add('endTransition');
+                window.setTimeout(() => cardbot[i].classList.remove('endTransition'), 300)
+            }
+        })
+    
+        cardbot[i].addEventListener('click', (e) => {
+            e.target.classList.add('keepTransition')
+            chosenPlayer(i, e.target.classList[1]);
+            if(cardplayer[i].classList.contains('keepTransition')) {
+                cardplayer[i].classList.remove('keepTransition');
+                cardplayer[i].classList.add('endTransition');
+                window.setTimeout(() => cardplayer[i].classList.remove('endTransition'), 300)
+            }
+        })
     }
-})
 
-    // if(e.target.id === 'player') {
-    //     if(previousTarget !== null) {
-    //         previousTarget.classList.remove('addBoxShadow');
-    //         previousTarget.style.color = 'black';
+    let chosenPlayer1 = null;
+    let chosenPlayer2 = null;
+    const chosenPlayer = (cardNumber, choice) =>{
+   
+        Number(cardNumber) === 0 ? chosenPlayer1 = choice : chosenPlayer2 = choice;
 
-    //     }
-    //     e.target.classList.add('addBoxShadow');
-    //     e.target.style.color = 'red';
-    //     previousTarget = e.target;
-    // } else if(e.target.id === 'bot') {
-    //     if(previousTarget !== null) {
-    //         previousTarget.classList.remove('addBoxShadow');
-    //         previousTarget.style.color = 'black';
+        console.log(chosenPlayer1, chosenPlayer2)
+    }
 
-    //     }
-    //     e.target.classList.add('addBoxShadow');
-    //     e.target.style.color = 'red'
-    //     previousTarget = e.target;
-    // }
+    const changeGif = (cardNumber, choice) => {
+        
+    }
+
+    
+
+   
+
+})()
